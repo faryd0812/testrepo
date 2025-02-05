f4e893c8e61277defff1c4ac51c5858ecd866cb0
Nilum <72471632+faryd0812@users.noreply.github.com>
2025-02-05 13:21:56 -0500

    Add files via upload
    DataScienceEcosystem for IBM coursera.

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objetives**
+
+# - List popular languages for Data Science  
+# - Identify commonly used Data Science libraries  
+# - Explore open-source development tools for Data Science  
+# - Perform basic arithmetic operations in Python  
+# - Convert time units using Python expressions 
+
+# Some of the popular languages that Data Scientists use are:
+#  - Python
+#  - R
+#  - SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# - Pandas – Para la manipulación y análisis de datos en Python.
+# - NumPy – Para operaciones matemáticas y manejo de arrays multidimensionales.
+# - Scikit-learn – Para aprendizaje automático y modelado estadístico.
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook  |
+# | RStudio          |
+# | Apache Zeppelin  |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# - **Addition**: 5 + 3 = 8
+# - **Subtraction**: 10 - 4 = 6
+# - **Multiplication**: 6 * 7 = 42
+# - **Division**: 20 / 5 = 4
+# - **Exponentiation**: 2 ** 3 = 8
+
+# In[1]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+print((3*4)+5)
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+min = 200
+min_hr = min/60
+
+print(min_hr)
+
+
+# ## Author
+# 
+# Faryd A. Arboleda C.
+
+# In[ ]:
+
+
+
+
